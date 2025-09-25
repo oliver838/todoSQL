80a1e7f52371f7f9f9a70a2ce3405218f6cb327c
unknown <oliver88gm@gmail.com>
2025-09-25 08:55:39 +0200

    bnkjfdny b

--- configDB.js
+++ configDB.js
@@ -1,6 +1,16 @@
-export const configDB = {
+import dotenv from 'dotenv'
+dotenv.config()
+export const configDB_ = {
   host: "localhost",
   user: "root",
   password: "",
   database: "todos",
+};
+
+export const configDB = {
+  host:process.env.DB_HOST,
+  user:process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  database:process.env.DB_DATABASE,
+  port:process.env.DB_PORT
 };
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import mysql from "mysql2/promise";
 const app = express();
 app.use(express.json());
 app.use(cors());
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 let connection;
 try {
